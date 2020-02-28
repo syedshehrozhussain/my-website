bbc72835b398acbee382e4b7ed624c8ce7f29337
Syed Shehroz Hussain <shehrozhussain@hotmail.com>
2020-02-28 17:07:24 -0500

    Updates

---
+++ precache-manifest.1e98bd6552a95d00590ccabcb6f8872c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7dc32f8573d8798af5b07d1fb76668c",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "100eda1b8aebca1f06d0",
+    "url": "/my-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a06b6da16647c0c2b4a",
+    "url": "/my-website/static/js/2.629be76b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-website/static/js/2.629be76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100eda1b8aebca1f06d0",
+    "url": "/my-website/static/js/main.ba31c1c4.chunk.js"
+  },
+  {
+    "revision": "4c6e45d2d7c5bb500512",
+    "url": "/my-website/static/js/runtime-main.7824eb4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
