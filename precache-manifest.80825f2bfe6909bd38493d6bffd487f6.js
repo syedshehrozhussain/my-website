92e8e36524a3f4afeabfcd66acece4a40fcda007
Syed Shehroz Hussain <shehrozhussain@hotmail.com>
2020-02-29 12:36:26 -0500

    Updates

---
+++ precache-manifest.80825f2bfe6909bd38493d6bffd487f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b21f0ac0bc937771640d883e76b1e63",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "471d3a845304bcdf28f4",
+    "url": "/my-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a06b6da16647c0c2b4a",
+    "url": "/my-website/static/js/2.629be76b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-website/static/js/2.629be76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471d3a845304bcdf28f4",
+    "url": "/my-website/static/js/main.52c27be6.chunk.js"
+  },
+  {
+    "revision": "4c6e45d2d7c5bb500512",
+    "url": "/my-website/static/js/runtime-main.7824eb4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7a354f842d0e8bef1eefc249dc02193a.js"
+  "/my-website/precache-manifest.80825f2bfe6909bd38493d6bffd487f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
