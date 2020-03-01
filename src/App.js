1d876a3aab3b878b906f76a887e1b9d95ea9c055
Syed Shehroz Hussain <shehrozhussain@hotmail.com>
2020-03-01 17:38:44 -0500

    reverting change after working page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter basename='/'>
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-            </ul>
-            <hr />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-        </div>
-    </HashRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Syed Shehroz Hussain's Space!
+        </p>
+      </header>
+    </div>
   );
 }
-
-const Home = () => <div><h2>Home</h2></div>
-const About = () => <div><h2>About</h2></div>
 
 export default App;
